e0175084c1c320c3a00a96aa17f344b00b3c05c1
NoviceCoder33 <anshu23.pandey@gmail.com>
2025-07-24 21:49:27 +0530

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); 
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://task-manager-3z7p.onrender.com'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
